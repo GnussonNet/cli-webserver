91cef82f450c450b7b2d468d908c22810302dd52
Gnusson.net <admin@gnusson.net>
2022-05-30 01:12:41 +0200

    👌 IMPROVE: Added jobs handling

--- src/main.js
+++ src/main.js
@@ -1,50 +1,24 @@
-import chalk from 'chalk';
-import fs from 'fs';
-import ncp from 'ncp';
-import path from 'path';
-import { promisify } from 'util';
-import Listr from 'listr';
+import { copyTemplate, startWebserver, reloadWebserver, stopWebserver, installCertificate } from './actions.js';
 
-const access = promisify(fs.access);
-const copy = promisify(ncp);
+let errors = 0;
 
-async function copyTemplateFiles(options) {
-  return copy(options.templateDirectory, options.targetDirectory, {
-    clobber: false,
-  });
-}
+export default async (options) => {
+  if (options.run.includes('start'))
+    await startWebserver(options).then((result) => {
+      if (result === false) errors++;
+    });
+  if (options.run.includes('reload'))
+    await reloadWebserver(options).then((result) => {
+      if (result === false) errors++;
+    });
+  if (options.run.includes('stop'))
+    await stopWebserver(options).then((result) => {
+      if (result === false) errors++;
+    });
+  if (options.run.includes('installCert'))
+    await installCertificate(options).then((result) => {
+      if (result === false) errors++;
+    });
 
-async function copyTemplate(options) {
-  options = {
-    ...options,
-    targetDirectory: options.targetDirectory || process.cwd(),
-  };
-
-  const templateDir = path.resolve(
-    new URL(import.meta.url).pathname,
-    '../../templates'
-    //  options.template || 'JavaScript' <----- makes it possible to have multiple templates folders
-  );
-  options.templateDirectory = templateDir;
-
-  try {
-    await access(templateDir, fs.constants.R_OK);
-  } catch (err) {
-    console.error('%s Something went wrong... could not find templates folder', chalk.red.bold('ERROR'));
-    process.exit(1);
-  }
-
-  const tasks = new Listr([
-    {
-      title: 'Copy template files',
-      task: () => copyTemplateFiles(options),
-      skip: () => (!options.template ? 'Pass --template to automatically copy template files' : undefined),
-    },
-  ]);
-
-  await tasks.run();
-  console.log('\n%s Project ready', chalk.green.bold('DONE'));
-  return true;
-}
-
-export { copyTemplate };
+  return errors;
+};
